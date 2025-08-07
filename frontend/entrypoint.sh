ea4270171632279b44b0de5b4028ba75e163ccdc
swagat-devops <swagatdevops@gmail.com>
2025-08-07 09:57:33 +0530

    commit

--- frontend/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Replace the placeholder in env.js with the actual backend URL from ConfigMap
-sed -i "s|EMPLOYEE_BACKEND_URL|$EMPLOYEE_BACKEND_URL|g" /usr/share/nginx/html/main*.js
-sed -i "s|EMPLOYEE_BACKEND_URL|$EMPLOYEE_BACKEND_URL|g" /usr/share/nginx/html/main*.js
-# Start Nginx
-nginx -g "daemon off;"
--- frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    baseURL: 'EMPLOYEE_BACKEND_URL'
-    //baseURL: 'http://sitcbs.com:32396/api/v1/employees'
-  };
-  
